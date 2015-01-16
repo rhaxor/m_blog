f8106fb80fe43fb17db217656d7d0c184b637617
rhaxor <rockking100@live.com>
2015-01-16 19:07:48 +0000

    Admin attribute

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}  
 
+
+
   
   resources :posts do
     resources :comments

6af9c7c2785fa255e909574524b2af1c527888a4
rhaxor <rockking100@live.com>
2015-01-16 17:00:21 +0000

    sign up disabled, admin panel link added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}  
+
+  
   resources :posts do
     resources :comments
   end
   
   root "posts#index"
   get '/about', to: 'pages#about'
+  
+
 end
